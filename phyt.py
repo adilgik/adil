816f80d985d20dc1f0626bcb683e6d9f2bf0d6b6
adilgik <111339373+adilgik@users.noreply.github.com>
2023-02-15 15:03:18 +0500

    Update phyt.py

--- phyt.py
+++ phyt.py
@@ -10,17 +10,17 @@ print('Hello, world!')
 num1 = 10
 num2 = 14
 num3 = 12
-
+largest=0
 # uncomment following lines to take three numbers from user
 #num1 = float(input("Enter first number: "))
 #num2 = float(input("Enter second number: "))
 #num3 = float(input("Enter third number: "))
 
-if (num1 >= num2) and (num1 >= num3):
+if (num1 >= largest):
    largest = num1
-elif (num2 >= num1) and (num2 >= num3):
+if (num2 >= largest)
    largest = num2
-else:
+if (num2 >= largest)
    largest = num3
 
 print("The largest number is", largest)
